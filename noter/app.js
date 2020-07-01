365b10c16438e4eda5922f73c7f6e6acb17b7407
Rizart Dokollari <rizart@hussle.com>
2020-07-01 15:32:31 +0100

    Install validator package.

--- noter/app.js
+++ noter/app.js
@@ -1,3 +1,7 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 console.log(getNotes())
+
+console.log(validator.isEmail('invalid'))
+console.log(validator.isEmail('r.dokollari@gmail.com'))
